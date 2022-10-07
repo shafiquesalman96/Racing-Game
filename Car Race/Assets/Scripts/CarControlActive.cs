8aca9dbad5357df1a2679e366616a3f69e73936a
Sulman Shafique <shafiquesalman96@gmail.com>
2022-10-07 15:52:30 +0200

    Countdown (Laptime instant start error)

---
+++ Car Race/Assets/Scripts/CarControlActive.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarControlActive : MonoBehaviour
+{
+    public GameObject CarControl;
+
+    void Start()
+    {
+        CarControl.GetComponent<CarController>().enabled = true;
+    }
+}
